2c43fa3a8d69913f7886a81a4c8c9cbe04fc937b
toastynerd <tylermorgan86@gmail.com>
2016-04-18 16:04:02 -0700

    and of apr 16

--- week_2/apr_18_express_basics/server.js
+++ week_2/apr_18_express_basics/server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
-app.get('/something', (req, res) => {
-  res.status(200).json({msg: 'hello world'});
+
+var ourRouter = express.Router();
+var routerCeption = express.Router();
+
+routerCeption.get('/time', (req, res) => {
+  res.status(200).json({msg: 'it\'s really really slow'});
 });
 
-app.get('/*', (req,res) => {
-  res.status(200).json({msg: 'wow such routes'});
-});
-
-app.get('/greet/:name', (req, res) => {
+ourRouter.get('/greet/:name', (req, res) => {
   res.status(200).json({msg: 'hello ' + req.params.name});
 });
+
+ourRouter.use('/anotherlevel', routerCeption);
+app.use('/api', ourRouter);
+
+ourRouter.get('/hello/world', (req, res) => {
+  res.status(200).json({msg: 'after the use'});
+});
+
+app.get('/api/*', (req, res) => {
+  res.status(200).json({msg: 'not in router'});
+});
+
 app.listen(3000, () => console.log('server up'));
