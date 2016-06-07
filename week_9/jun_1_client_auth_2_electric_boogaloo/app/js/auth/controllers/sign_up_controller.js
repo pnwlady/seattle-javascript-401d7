14e2f17450882e0cad2fe615e871224e195068d6
heidilaursen <heidimlaursen@gmail.com>
2016-06-06 22:09:58 -0700

    finished the sign up comment

--- week_9/jun_1_client_auth_2_electric_boogaloo/app/js/auth/controllers/sign_up_controller.js
+++ week_9/jun_1_client_auth_2_electric_boogaloo/app/js/auth/controllers/sign_up_controller.js
@@ -2,7 +2,8 @@ var baseUrl = require('../../config').baseUrl;
 module.exports = function(app) {
   app.controller('SignUpController', ['$http', '$location',  'cfHandleError', 'cfAuth', function($http, $location, handleError, auth) {
     // AUTH_EXP: how does this differ from the sign_in_controller
-    This button creates new user and will post the new username and password to 
+    // This button creates new user and the authentication passes in the baseUrl and api signip and user into a post request. Then auth service is run through the auth Controller and when the promise will throw an error or trigger the then callback, saving the new username and password with the url. 
+
     this.signup = true;
     this.errors = [];
     this.buttonText = 'Create New User!'
